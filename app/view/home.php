ec3d10161d8b0cdf8e347e14e1f7304abc8b7ec1
Laurine <peyrardlaurine@gmail.com>
2017-03-23 18:45:16 +0100

    Optimisation PHP de la muerte

--- app/view/home.php
+++ app/view/home.php
@@ -64,109 +64,53 @@
 			<div class="container">
 
 
-				<?php foreach ($project in $projects) {
+				<?php foreach ($projects as $project): ?> 
 					
-				} ?>
 
-				<div class="row project valign wrapper">
-					<div class="small-12 medium-6 large-4 columns">
-						<div class="title unveil" >
-							<a href="<?php echo PUBLIC_PATH ?>projet/0" data-hover="<?= $projects[0]->{'name'} ?>">
-								<?= $projects[0]->{'name'} ?>
-							</a>
-						</div>
-						<p><?= $projects[0]->{'description'} ?></p>
-					</div>
-					<div class="small-12 medium-6 large-8 columns align-right">
-						<a href="<?php echo PUBLIC_PATH ?>projet/0" data-hover="<?= $projects[0]->{'name'} ?>">
-							<div class="image project1" style="background-image: url('<?= PROJECT_PATH.$projects[0]->{'thumb'} ?>');">
-								<div class="filter">
-									<span>Voir le projet &#65515;</span>
+					<div class="row project valign wrapper <?= $project->{'id'}%2==0?'':'even' ?>">
+
+						<?php if($project->{'id'}%2 == 0): ?>
+							<div class="small-12 medium-6 large-4 columns">
+								<div class="title unveil" >
+									<a href="<?= PUBLIC_PATH ?>projet/<?= $project->{'id'} ?>" data-hover="<?= $project->{'name'} ?>">
+										<?= $project->{'name'} ?>
+									</a>
 								</div>
+								<p><?= $project->{'description'} ?></p>
 							</div>
-						</a>
-					</div>
-				</div>
+						<?php endif ?>
 
 
-				<div class="row project valign middle-project wrapper">
-					<div class="small-12 medium-6 large-8 columns align-left">
-						<a href="<?php echo PUBLIC_PATH ?>projet/1" data-hover="<?= $projects[1]->{'name'} ?>">
-							<div class="image project2" style="background-image: url('<?= PROJECT_PATH.$projects[1]->{'thumb'} ?>');">
-								<div class="filter">
-									<span>Voir le projet &#65515;</span>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="small-12 medium-6 large-4 columns">
-						<div class="title text-right unveil">
-							<a href="<?php echo PUBLIC_PATH ?>projet/1" data-hover="<?= $projects[1]->{'name'} ?>">
-								<?= $projects[1]->{'name'} ?>
-							</a>
-						</div>
-						<p class="right"><?= $projects[1]->{'description'} ?></p>
-					</div>
-				</div>
-
-
-
-				<div class="row project valign wrapper">
-					<div class="small-12 medium-6 large-4 columns">
-						<div class="title unveil">
-							<a href="<?php echo PUBLIC_PATH ?>projet/2"  data-hover="<?= $projects[2]->{'name'} ?>">
-								<?= $projects[2]->{'name'} ?></div>
-							</a>
-							<p><?= $projects[2]->{'description'} ?></p>
-						</div>
-						<div class="small-12 medium-6 large-8 columns align-right">
-							<a href="<?php echo PUBLIC_PATH ?>projet/2"  data-hover="<?= $projects[2]->{'name'} ?>">
-								<div class="image project3" style="background-image: url('<?= PROJECT_PATH.$projects[2]->{'thumb'} ?>');">
+						<div class="small-12 medium-6 large-8 columns 
+						<?= $project->{'id'}%2==0?'align-right':'align-left' ?>">
+							<a href="<?= PUBLIC_PATH ?>projet/<?= $project->{'id'} ?>" data-hover="<?= $project->{'name'} ?>">
+								<div class="image project1" style="background-image: url('<?= PROJECT_PATH.$project->{'thumb'} ?>');">
 									<div class="filter">
 										<span>Voir le projet &#65515;</span>
 									</div>
 								</div>
 							</a>
 						</div>
-					</div>
 
-
-					<div class="row">
-						<hr>
-					</div>
-
-
-					<div class="row" id="thumb-row">
-						<a href="<?php echo PUBLIC_PATH ?>projet/3">
-							<div class="small-12 medium-6 large-4 columns thumbnail">
-								<div class="image project4" style="background-image: url('<?= PROJECT_PATH.$projects[3]->{'thumb'} ?>');">
-									<div class="title"><?= $projects[3]->{'name'} ?></div>
+						<?php if($project->{'id'}%2 == 1): ?>
+							<div class="small-12 medium-6 large-4 columns">
+								<div class="title unveil" >
+									<a href="<?= PUBLIC_PATH ?>projet/<?= $project->{'id'} ?>" data-hover="<?= $project->{'name'} ?>">
+										<?= $project->{'name'} ?>
+									</a>
 								</div>
+								<p><?= $project->{'description'} ?></p>
 							</div>
-						</a>
+						<?php endif ?>
 
-						<a href="<?php echo PUBLIC_PATH ?>projet/4">
-							<div class="small-12 medium-6 large-4 columns thumbnail" >
-								<div class="image project5" style="background-image: url('<?= PROJECT_PATH.$projects[4]->{'thumb'} ?>');">
-									<div class="title"><?= $projects[4]->{'name'} ?></div>
-								</div>
-							</div>
-						</a>
-
-						<a href="<?php echo PUBLIC_PATH ?>projet/5">
-							<div class="small-12 medium-6 large-4 columns thumbnail" >
-								<div class="image project6" style="background-image: url('<?= PROJECT_PATH.$projects[5]->{'thumb'} ?>');">
-									<div class="title"><?= $projects[5]->{'name'} ?></div>
-								</div>
-							</div>
-						</a>
 
 					</div>
+				<?php endforeach ?>
 
-				</div>
 			</div>
+		</div>
 
-			<?php include('template/footer.php') ?>
+		<?php include('template/footer.php') ?>
 
-		</body>
-		</html>
+	</body>
+</html>
